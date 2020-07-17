7aa1e745046a2ee9b14a219fe5b60cf5c4002dd2
LeandroLFE <lfe.leandro@gmail.com>
2020-07-17 13:19:15 -0300

    exercicio do final da aula 5

--- app_restful.py
+++ app_restful.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-from habilidades import Habilidades, Habilidade
+from habilidades import Habilidades, Habilidade, lista_habilidades
 
 import json
 
@@ -38,8 +38,15 @@ class Desenvolvedor(Resource):
 
     def put(self, id):
         dados = json.loads(request.data)
-        desenvolvedores[id] = dados
-        return dados
+        if dados["habilidades"] not in lista_habilidades:
+            retorno = {
+                'status': 'erro',
+                'mensagem': 'Habilidade não existente na lista'
+            }
+            return retorno
+        else:
+            desenvolvedores[id] = dados
+            return dados
 
     def delete(self, id):
         desenvolvedores.pop(id)
@@ -56,8 +63,26 @@ class Lista_Desenvolvedores(Resource):
         novo_desenvolvedor = json.loads(request.data)
         posicao = len(desenvolvedores)
         novo_desenvolvedor["id"] = posicao
-        desenvolvedores.append(novo_desenvolvedor)
-        return desenvolvedores[posicao]
+
+
+        tamanho_atual = len(lista_habilidades)
+        tamanho_add = len(novo_desenvolvedor["habilidades"])
+        nova_habilidade = novo_desenvolvedor["habilidades"]
+        permissao_add = True
+
+        for i in range(tamanho_add):
+            if(nova_habilidade[i] not in lista_habilidades):
+                permissao_add = False
+
+        if not(permissao_add):
+            retorno = {
+                'status': 'erro',
+                'mensagem': 'Habilidade não existente na lista'
+            }
+            return retorno
+        else:
+            desenvolvedores.append(novo_desenvolvedor)
+            return desenvolvedores[posicao]
 
 api.add_resource(Desenvolvedor, '/dev/<int:id>')
 api.add_resource(Lista_Desenvolvedores, '/dev/')
